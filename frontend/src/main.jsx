182f29162d2dadb9122e475df15df82943404ce4
Yash Patel <yasupatel219@gmail.com>
2025-09-05 15:12:08 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 
 
 // index.js
