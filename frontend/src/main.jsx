ffb0c26ebc9c1cfac6dc045358fa7781d953897e
Yash Patel <yasupatel219@gmail.com>
2025-09-05 15:23:14 +0530

    push

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
 
 
-// index.js
+import { HashRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-
+  <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
-
+  </React.StrictMode>
 );
+
